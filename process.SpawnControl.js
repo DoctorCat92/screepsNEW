9051fd72e069545c5af96b1843cbba393c4be836
DoctorCat92 <32587399+DoctorCat92@users.noreply.github.com>
2022-08-29 12:05:18 +0700

    1
    1

--- process.SpawnControl.js
+++ process.SpawnControl.js
@@ -405,7 +405,8 @@ var processSpawnControl = {
                 let RoomMass =_.filter(Game.rooms,rooms=>rooms.controller&&rooms.controller.my);
                 let DistanceRoom = 1000;
                 for (room in RoomMass) {
-                    let SquadFlag = Game.map.getRoomLinearDistance(SquadFlag.pos.roomName, RoomMass[room].name); // 3
+                    console.log(SquadFlag.pos.roomName);
+                    //let SquadFlag = Game.map.getRoomLinearDistance(SquadFlag.pos.roomName, RoomMass[room].name); // 3
                 /**    if (DistanceRoom > SquadFlag) {
                         DistanceRoom = SquadFlag;
                     } */
