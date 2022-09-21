d602e833fad5760b6bcb73e33ebe38642ae98743
DoctorCat92 <32587399+DoctorCat92@users.noreply.github.com>
2022-09-21 12:51:52 +0700

    1
    1

--- process.SpawnControl.js
+++ process.SpawnControl.js
@@ -421,7 +421,11 @@ var processSpawnControl = {
                         let FlagCheckPoint  =  _.find(Game.flags, f => f.name.startsWith('CheckPoint'+ResultRoom));
                         
                         if (FlagCheckPoint) {
-                            Game.rooms[ResultRoom].createFlag(FlagCheckPoint.pos.x, FlagCheckPoint.pos.y, 'CreateSquad Healer-3 Melee-2 Auto', SquadFlag.color);
+                            var ColorFlag = SquadFlag.indexOf('Squad');
+                            var str = SquadFlag.name;
+                            var NumberColor = str.slice(ColorFlag+5, 7);
+                            var ColorVar = Number(str[NumberColor]);
+                            Game.rooms[ResultRoom].createFlag(FlagCheckPoint.pos.x, FlagCheckPoint.pos.y, 'CreateSquad Healer-3 Melee-2 Auto', ColorVar);
                         }
                     }
                 }
