3f6f5d3c25e653a7d2fefc2b4a0edfbab6f7df81
DoctorCat92 <32587399+DoctorCat92@users.noreply.github.com>
2022-10-04 14:55:05 +0700

    1
    1

--- role.repairer.js
+++ role.repairer.js
@@ -96,9 +96,8 @@ var roleRepairer = {
         if (!creep.memory.repairer && !creep.memory.building) {
             var sources = creep.pos.findClosestByPath(FIND_SOURCES);
             var Storage = creep.room.find(FIND_STRUCTURES, {filter: structure => ((structure.structureType == STRUCTURE_STORAGE) && (structure.store[RESOURCE_ENERGY] > 600000))});
-            //
-            creep.say(Storage[0].id);
-            if (Storage[0]) { creep.say('Пипа');
+            
+            if (Storage[0]) { 
                 if(creep.withdraw(Storage[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.travelTo(Storage[0]);
                 }
