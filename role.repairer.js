b223a21210ce34533183881513a220671d75e92b
DoctorCat92 <32587399+DoctorCat92@users.noreply.github.com>
2022-10-04 14:46:25 +0700

    1
    1

--- role.repairer.js
+++ role.repairer.js
@@ -92,7 +92,7 @@ var roleRepairer = {
                 }
                 if ((buildTarget.length == 1) && (buildTarget[0].progress >= buildTarget[0].progressTotal - 50)) {Game.notify('Creeps complited construction in room '+creep.pos.roomName, 0);}
             }
-        } 
+        } creep.say('Пиписька');
         if (!creep.memory.repairer && !creep.memory.building) {
             var sources = creep.pos.findClosestByPath(FIND_SOURCES);
             var Storage = creep.room.find(FIND_CONSTRUCTION_SITES, {filter: structure => ((structure.structureType == STRUCTURE_STORAGE) && (structure.store[RESOURCE_ENERGY] > 500000))});
