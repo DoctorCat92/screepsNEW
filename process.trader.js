5c635e7e0740bd59919d9580e318cbdc3e10d075
DoctorCat92 <32587399+DoctorCat92@users.noreply.github.com>
2022-10-30 21:40:46 +0700

    1
    1

--- process.trader.js
+++ process.trader.js
@@ -93,7 +93,7 @@ var processTrader = {
                 console.log('Колличество ордеров по цене '+ Orders.length);
                 
                 if (Res == 'pixel' || Res == 'token' || Res == 'cpuUnlock' || Res == 'accessKey') {
-                    if (Game.market.deal(OrdersSort[0].id, OrdersSort[0].amount ,posTerminal) == OK) {
+                    if (Game.market.deal(OrdersSort[0].id, Amount ,posTerminal) == OK) {
                         Game.notify('Куплено '+Amount+' '+OrdersSort[0].resourceType+' по цене '+OrdersSort[0].price, 0);            
                        // Buy.remove();
                     } else {console.log('Сделка завершилась кодом '+Game.market.deal(OrdersSort[0].id, OrdersSort[0].amount ,posTerminal)+' Ордер '+OrdersSort[0].id);}
