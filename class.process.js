191697d43003e678afb446152574232bc170f2d7
DoctorCat92 <32587399+DoctorCat92@users.noreply.github.com>
2023-01-08 23:42:03 +0700

    123

--- class.process.js
+++ class.process.js
@@ -1,22 +1,29 @@
 class process {
 
-    constructor(n,p) {
-        this.name = n;
-        this.priority = p;
+    constructor(Name,nameModule,state,prority,roomNumber,memory) {
+        this.Name = Name;
+        this.NameProcess = Name+roomNumber;
+        this.nameModule = nameModule;
+        this.Status = state, 
+        this.Room = roomNumber, 
+        this.Memory= memory,
+        this.BusyTarget = [], 
+        this.List = {}, 
+        this.time = 0,
     }
 
-    start () {
+    start () { 
         if (Memory.processor === undefined) {
             Memory.processor = {};
         }
 
-        if (Memory.processor[this.name] === undefined) { 
-            Memory.processor[this.name] = {Name: this.name,  folder:{}, time: 0}; //Opt:this.opt,
+        if (Memory.processor[this.Name] === undefined) { 
+            Memory.processor[this.Name] = {Name: Name,NameProcess: Name, NameModule: nameModule, Status: true, Room: roomNumber, BusyTarget:[], List:{}, time: 0, Memory:memory}; //Opt:this.opt,
         }
     }
 
     stop () {
-        delete Memory.processor[this.name];
+        delete Memory.processor[this.Name];
     }
 }
 
