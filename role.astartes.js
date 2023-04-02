51a9a1a2ec188acfa75f099e441201790b7759e6
DoctorCat92 <vasyadmitr@gmail.com>
2023-04-02 21:42:39 +0700

    Astartes

--- role.astartes.js
+++ role.astartes.js
@@ -447,17 +447,17 @@ let roleAstartes = {
                                     let RangedBodyparts = creep.getActiveBodyparts(RANGED_ATTACK);
                                     let AttackBodyparts = creep.getActiveBodyparts(ATTACK);
                                     let AllAttackBodyparts = _.filter(creep.body, a => a.type == ATTACK || a.type == RANGED_ATTACK);
-                                    let Percent = Math.round((targetBanks.hits/targetBanks.hitsMax)*1000)/10;
+                                    let Percent = Math.round((targetBanks.hits/targetBanks.hitsMax)*1000)/100;
                                     // Графонистые плюшки
                                     
                                     new RoomVisual(creep.pos.roomName).rect(targetBanks.pos.x-2.5, targetBanks.pos.y-7, 5, 4, {fill:'#000000', opacity: 0.5});
                                     new RoomVisual(creep.pos.roomName).text('hits '+targetBanks.hits, targetBanks.pos.x-2, targetBanks.pos.y-6, {align: 'left'}); 
                                     new RoomVisual(creep.pos.roomName).text('% '+Percent, targetBanks.pos.x-2, targetBanks.pos.y-5, {align: 'left'}); 
-                                    if (creep.memory.hitsPowerBank !== undefined) {
-                                        
-                                        new RoomVisual(creep.pos.roomName).text('% '+Percent, targetBanks.pos.x-2, targetBanks.pos.y-4, {align: 'left'}); 
+                                    if (creep.memory.PersentHit !== undefined) {
+                                        let OldPersent = creep.memory.PersentHit-Percent;
+                                        new RoomVisual(creep.pos.roomName).text('% '+OldPersent, targetBanks.pos.x-2, targetBanks.pos.y-4, {align: 'left'}); 
                                     }
-                                    creep.memory.hitsPowerBank = targetBanks.hits;
+                                    creep.memory.PersentHit = Percent;
                                     //выбор частей тела
                                     
                                     if (AllAttackBodyparts.length !== 0) {
