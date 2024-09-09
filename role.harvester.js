7512fcf5074b21767a2f94ee0c08278ca2eff04b
Vasiliy <vasyadmitr@gmail.com>
2024-09-09 17:36:54 +0700

    harvester

--- role.harvester.js
+++ role.harvester.js
@@ -11,7 +11,7 @@ run: function(creep) {
             filter: (structure) => {
                 return (structure.structureType == STRUCTURE_SPAWN ||
                         structure.structureType == STRUCTURE_EXTENSION ||
-                        structure.structureType == STRUCTURE_TOWER) && (structure.store[RESOURCE_ENERGY] < structure.store.getCapacity());
+                        structure.structureType == STRUCTURE_TOWER) && (structure.store[RESOURCE_ENERGY] < structure.store.getCapacity(RESOURCE_ENERGY));
             }
         }); 
 
